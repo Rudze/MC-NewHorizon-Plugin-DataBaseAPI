b3b3bb5c3f5089af0ea655bad4bf52fcba1dcdae
Rudy <rudydavid1708@gmail.com>
2025-07-26 22:26:43 +0200

    edit:Modification du nom du package

--- src/main/java/fr/rudy/databaseapi/DatabaseManager.java
+++ src/main/java/fr/rudy/databaseapi/DatabaseManager.java
@@ -1,4 +1,4 @@
-package fr.rudy.dataBaseAPI;
+package fr.rudy.databaseapi;
 
 import java.io.File;
 import java.sql.Connection;
--- src/main/java/fr/rudy/databaseapi/main.java
+++ src/main/java/fr/rudy/databaseapi/main.java
@@ -1,4 +1,4 @@
-package fr.rudy.dataBaseAPI;
+package fr.rudy.databaseapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
